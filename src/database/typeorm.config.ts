c266fb0f8745b2a1306be4c81f991ffcd90c00ac
Oscar Ruiz <oscarruiz2614@gmail.com>
2025-06-13 18:00:03 -0500

    feat: database
    config connection database

---
+++ src/database/typeorm.config.ts
@@ -0,0 +1,19 @@
+import * as dotenv from 'dotenv';
+import { DataSource } from 'typeorm';
+
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [],
+  synchronize: false,
+  logging: true,
+  migrationsRun: false,
+  migrationsTableName: 'migrations',
+  migrations: [__dirname + '/migrations/*.{ts,js}'],
+});
